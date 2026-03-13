f818bde27bf7ac1e95283f292e311ea904d70aa5
Roberto <robertosan97@gmail.com>
2026-03-13 16:32:08 +0100

    build apptainer in two steps first solves env and second locks it

--- apptainer/build.sh
+++ apptainer/build.sh
@@ -3,4 +3,20 @@
 #SBATCH --mem=16G
 #SBATCH --output=build.log
 
+# If Apptainer_builder.sif doesn't exist, build it
+
+if [ ! -f "Apptainer_builder.sif" ]; then
+    echo "Building builder container"
+    apptainer build Apptainer_builder.sif Apptainer_builder.def
+fi
+
+# Export locked environments
+echo "Exporting locked environment"
+
+apptainer exec Apptainer_builder.sif /opt/conda/bin/conda env export -n base > environment.yml
+
+echo "Building definitive container"
+# Build the Apptainer container
 apptainer build --force Apptainer.sif Apptainer.def
+
+
